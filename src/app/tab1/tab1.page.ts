db6e1e8496358503047851d4e358f0110d9151e9
Juancruzd <42796029+Juancruzd@users.noreply.github.com>
2020-03-28 14:20:52 -0600

    updates

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -5,8 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 export class Tab1Page {
+  uno = '/assets/img/numeros/1.jpg';
+  dos = '/assets/img/numeros/2.jpg';
+  tres = '/assets/img/numeros/3.jpg';
+  cuatro = '/assets/img/numeros/4.jpg';
+  constructor() {
 
-  constructor() {}
+  }
+  addEvent(){
+    
 
+    
+  }
 }
